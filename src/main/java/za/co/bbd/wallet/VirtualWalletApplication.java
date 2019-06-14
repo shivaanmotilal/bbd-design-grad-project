39155227be7bf374827ac1705036d7520cb4c21f
Rowan Reeve <rowanreeve@gmail.com>
2019-06-14 19:13:21 +0200

    Initial commit

---
+++ src/main/java/za/co/bbd/wallet/VirtualWalletApplication.java
@@ -0,0 +1,13 @@
+package za.co.bbd.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/bbd/wallet/VirtualWalletApplicationTests.java
@@ -0,0 +1,16 @@
+package za.co.bbd.wallet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VirtualWalletApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
