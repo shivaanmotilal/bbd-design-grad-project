d52de73abf760a0d0ab09361ebc0c10dfdff10c1
Lauren Barger <laurenbarger@discovery.bank>
2019-07-05 04:43:17 +0200

    MVP auth & settle

---
+++ src/main/java/za/co/bbd/wallet/exceptions/UnauthorizedException.java
@@ -0,0 +1,10 @@
+package za.co.bbd.wallet.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends Exception {
+    public UnauthorizedException() { super(); }
+    public UnauthorizedException(String message) { super(message); }
+}
